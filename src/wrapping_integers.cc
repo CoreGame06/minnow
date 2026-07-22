09ba90ab785b4c9585a4162c152911c5e90ac339
22813 <2281388156@qq.com>
2026-07-22 10:19:52 +0800

    Complete wrapping，add files in gitignore

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,14 +5,20 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  // 先64->32(相当于取模)，相加后uint32自动取模(溢出)
+  // (a+b) mod M = [(a mol M) + (b mol M)] mol M
+  return zero_point+static_cast<uint32_t>(n);
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  // L = 2**32，则候选数为 offset+K*L
+  uint32_t offset = raw_value_ - zero_point.raw_value_;
+  // 我们要找的就是距离上一个 checkpoint 最近的候选数
+  uint32_t ck_low = static_cast<uint32_t>(checkpoint);
+  // trick
+  int diff = static_cast<int32_t>(offset-ck_low); 
+  int64_t res = diff + static_cast<int64_t>(checkpoint);
+
+  return res<0? static_cast<uint64_t>(res+(1ULL<<32)):static_cast<uint64_t>(res);
 }
