0c037232c64b5e711d9106e1d6b93edcf5cc95be
22813 <2281388156@qq.com>
2026-07-24 18:26:40 +0800

    Complete lab6

--- src/router.cc
+++ src/router.cc
@@ -20,11 +20,79 @@ void Router::add_route( const uint32_t route_prefix,
        << static_cast<int>( prefix_length ) << " => " << ( next_hop.has_value() ? next_hop->ip() : "(direct)" )
        << " on interface " << interface_num << "\n";
 
-  debug( "unimplemented add_route() called" );
+  routing_table_.emplace_back(route_prefix,prefix_length,next_hop,interface_num);//原地构造，省区一次拷贝，性能最好
+}
+
+std::optional<Router::RouteEntry> Router::match_route(const uint32_t dst_ip) const
+{
+  const RouteEntry* best_route = nullptr;
+  int max_prefix_length = -1;
+
+  for(const auto& route:routing_table_)
+  {
+    bool is_match = false;
+
+    // 当前prefixlength=0，则任意IP都匹配
+    if(route.prefix_length==0)
+      is_match = true;
+    else
+    {
+      // 构造掩码
+      uint32_t mask = 0xFFFFFFFFu << (32-route.prefix_length);
+      is_match = ((dst_ip & mask) == (route.route_prefix & mask));
+    }
+    // 最长匹配
+    if(is_match && static_cast<int>(route.prefix_length)>max_prefix_length)
+    {
+      max_prefix_length = route.prefix_length;
+      best_route = &route;
+    }
+  }
+
+  return best_route? std::make_optional(*best_route):std::nullopt;
+
+}
+
+void Router::route_datagram(InternetDatagram& dgram)
+{
+  const auto route_opt = match_route(dgram.header.dst);
+  if(!route_opt.has_value())
+    return;
+  
+  // 检查TTL
+  if(dgram.header.ttl<=1)
+    return;
+
+  // TTL减少，重新计算校验码
+  dgram.header.ttl--;
+  dgram.header.compute_checksum();
+
+  //确定 Next Hop IP
+  const auto& route = route_opt.value();
+  const Address next_hop_addr = route.next_hop.has_value()? 
+                                route.next_hop.value()
+                                :Address::from_ipv4_numeric(dgram.header.dst);
+
+  interface(route.interface_num)->send_datagram(dgram,next_hop_addr);
 }
 
 // Go through all the interfaces, and route every incoming datagram to its proper outgoing interface.
 void Router::route()
 {
-  debug( "unimplemented route() called" );
+  // 1.讲网卡积压的数据包拿出来
+  // 2.查找手册
+  // 3.减少TTL
+  // 4.发送
+  for(auto& network_interface:interfaces_)
+  {
+    auto& in_queue = network_interface->datagrams_received();
+    while(!in_queue.empty())
+    {
+      InternetDatagram dgram = move(in_queue.front());
+      in_queue.pop();
+
+      // 单包转发
+      route_datagram(dgram);
+    }
+  }
 }
--- src/router.hh
+++ src/router.hh
@@ -4,7 +4,7 @@
 #include "network_interface.hh"
 
 #include <optional>
-
+#include <vector>
 // \brief A router that has multiple network interfaces and
 // performs longest-prefix-match routing between them.
 class Router
@@ -33,5 +33,24 @@ public:
 
 private:
   // The router's collection of network interfaces
-  std::vector<std::shared_ptr<NetworkInterface>> interfaces_ {};
+  std::vector<std::shared_ptr<NetworkInterface>> interfaces_ {}; // 每一个就是一个插在这个路由器上，由这个路由器管理的网卡
+
+  // 路由表单个的结构体
+  struct RouteEntry
+  {
+    uint32_t route_prefix; //路由前缀IP
+    uint8_t prefix_length;  // 前缀匹配长度
+    std::optional<Address> next_hop; // 下一跳IP地址，若为空，则目的地就是IP包本身的dst
+    size_t interface_num; // 出口网卡接口的索引号
+  };
+
+  // 路由表：存储转发规则
+  std::vector<RouteEntry> routing_table_{};
+
+  // 辅助函数
+  // 找到最匹配的 RouteEntry
+  std::optional<Router::RouteEntry> match_route( const uint32_t dst_ip ) const;
+  // 处理单个数据包的dgram,处理发送前的准备工作
+  void route_datagram( InternetDatagram& dgram );
+
 };
